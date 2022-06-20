30da846a2706f161fd37dc6cb5e263d269aad495
chengyiyi <qq799996991@163.com>
2022-06-20 20:03:55 +0800

    hot-fix commit1

--- git-demo01/src/main/java/com/test/git/GitTest.java
+++ git-demo01/src/main/java/com/test/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git！");
         System.out.println("Hello Hello Git！");
         System.out.println("Hello Hello Hello Git！");
+        System.out.println("Hello Hello Hello Hello Git！");
     }
 }
